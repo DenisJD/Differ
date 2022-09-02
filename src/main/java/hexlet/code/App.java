88664f012cf4118937308f09c71d8797825d6fee
DenisJD <t.denisjd@gmail.com>
2022-09-02 12:01:22 +0300

    create "Help (-h)"

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "picocli 4.6.3",
+        description = "Compares two configuration files and shows a difference.")
+class App {
+
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish",
+            paramLabel = "format")
+    String format;
+
+    @Parameters (paramLabel = "filepath1", description = "path to first file")
+    File filepath1;
+
+    @Parameters (paramLabel = "filepath2", description = "path to second file")
+    File filepath2;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
